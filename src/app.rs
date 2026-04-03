a83cb82a7a329da316e78293ce4478ecb3e8fee2
Egor Kosachev <egorkosachev@vk.com>
2026-04-03 23:36:59 +0300

    initial

---
+++ src/app.rs
@@ -0,0 +1,8 @@
+use eframe::App;
+
+#[derive(Default)]
+pub struct MaxFlowProblemApp;
+
+impl App for MaxFlowProblemApp {
+    fn ui(&mut self, _ui: &mut eframe::egui::Ui, _frame: &mut eframe::Frame) {}
+}
---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use crate::app::MaxFlowProblemApp;
+
+mod app;
+
+fn main() {
+    let native_options = eframe::NativeOptions::default();
+    let _ = eframe::run_native(
+        "Max Flow Problem",
+        native_options,
+        Box::new(|_cc| Ok(Box::new(MaxFlowProblemApp::default()))),
+    );
+}
